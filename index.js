b5552958bba4d8431244dab8610e74dd42c64f69
Sabino Capingana <77695364+Capingana@users.noreply.github.com>
2024-02-14 18:40:56 +0100

    yes

--- index.js
+++ index.js
@@ -2,18 +2,37 @@ const express = require("express");
 const app = express();
 const PORTA = 8081;
 
-// ===================================================
-app.get("/", (req, response) => {
-    let sms="Estou dentro do meu app node.js";
-  response.send(req.params);
+// ROUTER===========================================
+app.get("/", function (requisition, response) {
+  const Person = [
+    {
+      id: 1,
+      ifirstName: "Sabino",
+      lastName: "Capingana",
+      email: "scapingana@gmail.com",
+      telefone: 4567,
+    },
+    {
+      id: 2,
+      ifirstName: "Joé",
+      lastName: "Capingana",
+      email: "jose@gmail.com",
+      telefone: 4567,
+    },
+    {
+      id: 3,
+      ifirstName: "Pedro",
+      lastName: "António",
+      email: "antónio@gmail.com",
+      telefone: 4567,
+    },
+  ];
+  const personJson=JSON.stringify(Person);
+  const personList=JSON.parse(personJson);
+  response.send(personList);
 });
 
-// ===================================================
-app.get("/sobre",function(req,response){
-response.send("Pagina sobre");
-});
-
-// CRIANDO SERVIDOR
+// LOCALHOST==================================================
 app.listen(PORTA, () => {
-  console.log(`The server is running in : ${PORTA}`);
+  console.log(`O SERVIDOR ESTÁ RODANDO NA PORTA ${PORTA}`);
 });
